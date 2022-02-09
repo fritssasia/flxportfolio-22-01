fa9c26425b3f1310d22e2ee02353813f8057c1a6
Rivas Frits Sasia <fritssasia@gmail.com>
2022-02-10 06:56:20 +0800

    update

---
+++ asset/js/script.js
@@ -0,0 +1,23 @@
+// Wait for document to load
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.documentElement.setAttribute("data-theme", "light");
+
+    // Select switch button
+    var themeSwitcher = document.getElementById("btnSwithTheme");
+
+    // When our button gets clicked
+    themeSwitcher.onclick = function () {
+        // Get the current selected theme, on the first run
+        // it should be `light`
+        var currentTheme = document.documentElement.getAttribute("data-theme");
+
+        // Switch between `dark` and `light`
+        var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+        // Set our currenet theme to the new one
+        document.documentElement.setAttribute("data-theme", switchToTheme);
+
+        var z = document.getElementById('btnSwithTheme');
+        z.classList.toggle('checked');
+    }
+});
